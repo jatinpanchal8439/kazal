f059028453cb01d960c1756c9f9f40dcae1ee39b
jatinpanchal8439 <jatinvishwakarma619@gmail.com>
2026-05-01 16:46:31 +0530

    Fix 404 error - correct TanStack Start main.tsx and add proper vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { getRouter } from "./router";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = getRouter();
+
+const rootElement = document.getElementById("root")!;
+router.mount({
+  rootElement,
+});
